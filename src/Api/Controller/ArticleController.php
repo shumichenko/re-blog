01aa2e12d3cc5e284eb720386937fca4e6ad4946
shumichenko <s6174401@mail.ru>
2020-09-03 21:34:39 +0300

    Some revisions

--- src/Api/Controller/ArticleController.php
+++ src/Api/Controller/ArticleController.php
@@ -40,7 +40,9 @@ class ArticleController extends AbstractController
     public function getListOfArticles(ArticleFetcher $articleFetcher) : JsonResponse
     {
         $articles = $articleFetcher->getSerializedArticles();
-        return $this->respond($articles);
+        return empty($articles) ?
+            $this->respond(JsonResponse::HTTP_NO_CONTENT) :
+            $this->respond($articles);
     }
 
     /**
